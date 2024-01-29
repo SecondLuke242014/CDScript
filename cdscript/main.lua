1fea705a700c29963abe853b7ccfa865fec48353
LukeOfficial <e87624273@gmail.com>
2024-01-29 16:44:49 +0400

    Update main.lua

--- cdscript/main.lua
+++ cdscript/main.lua
@@ -1,1 +1,20 @@
+--[[
+COPYRIGHT CDS 2024
+The CDScript programming language
+]]
 
+local cds = require('cds')
+
+function compile(code)
+  for i,v in pairs(string.split(code,' ')) do
+    if v == 'output' then
+      print('CDScript: '..split[i+1])
+    elseif v == 'warn'
+      warn('CDScript: '..split[i+1])
+    elseif v == 'halt'
+      error('CDScript halted: '..split[i+1])
+    end
+  end
+end
+
+compile(cds)
